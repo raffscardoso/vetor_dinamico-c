1fdf14576df21016ca96ba69875bcc909676fba7
raffs <raffs.cardoso15@gmail.com>
2025-06-03 22:27:56 -0300

    esqueleto do projeto, prototipo das funcoes

---
+++ vetor_dinamico.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  {
+  void* array;
+  size_t capacidade;
+  size_t tamanho_atual;
+  size_t tamanho_elemento;
+} VetorDinamico;
+
+VetorDinamico* criar_vetor(size_t tamanho_elemento); //aloca e inicializa um novo vetor dinamico
+void destruir_vetor(VetorDinamico* vetor, void (*liberar_elemento)(void*)); //libera toda a memoria alocada pelo vetor, usar liberar_elemento para desalocar memoria de cada elemento individualmente (ex: vetor de strings)
+void adicionar_elemento(VetorDinamico* vetor, void* elemento); // adiciona um elemento ao final do vetor. verificar capacidade e realocar caso necesario. memcpy para copiar elemento
+void* obter_elemento(VetorDinamico* vetor, size_t indice);
+void remover_elemento(VetorDinamico* vetor, size_t indice, void (*liberar_elemento)(void*)); // remove o elemento do indice e libera memoria com liberar_elemento
+void redimensionar_vetor(VetorDinamico* vetor, size_t indice, size_t nova_capacidade);
+
+
+int main(int argc, char *argv[])
+{
+
+  
+  return EXIT_SUCCESS;
+}
